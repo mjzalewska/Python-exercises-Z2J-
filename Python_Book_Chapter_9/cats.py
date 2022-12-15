be447aa6bb91990324d2b712ea94863c9bc3c85b
magdazalewska <m.jarzebska.zalewska@gmail.com>
2022-12-15 17:00:24 +0100

    Added file for challenge 9.9

---
+++ Python_Book_Chapter_9/cats.py
@@ -0,0 +1,18 @@
+"""
+Challenge 9.9
+
+Always start with the cat #1. Each time you stop at a cat,you check if it has a hat on. If not - you put one on,
+if it does - you take it off.
+Round #1: stop at every cat, placing a hat on each one
+Round #2: stop at every second cat (#2, #4, #6, #8, etc.)
+Round #3: stop at every third cat (#3, #6, #9, #12, etc.)
+Continue the process until you've made 100 rounds around the cats.
+On the last round, you stop only at cat #100.
+
+Write a program which outputs which cats have hats at the end.
+"""
+
+# 100 cats,
+# 100 rounds
+# first round - add a hat to each cat
+
