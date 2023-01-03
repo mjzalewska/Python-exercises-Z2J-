3c08105f5d40e62e8f3da48561be2b6ccc58d554
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-01-03 21:52:17 +0100

    listed basic pathlib commands

--- Python_Book_Chapter_12/exercises.py
+++ Python_Book_Chapter_12/exercises.py
@@ -10,5 +10,27 @@ currend_wd = pathlib.Path.cwd()
 print(currend_wd)
 print(type(currend_wd))
 
+# adding subdirectories
+print(home/'Downloads'/'all-coding-books.txt')
+# checking if path is absolute
+print(home.is_absolute())
+# extending a relative path
+print(home/pathlib.Path('Downloads/test_adv.txt'))
+# checking parent directories
+print(list(home.parents))
+# checking the root directory
+print(home.anchor)
+# checking name of file or directory
+print(home.name)
+# checking the stem(name) of a file
+print(path.stem)
+# checking file extension
+print(path.suffix)
 
-
+# checking if a path exists
+print(home.exists())
+# checking if path refers to a file or a directory
+# returns False if the file doesn't exist
+print(path.is_file())
+# checking if path refers to a dir
+print(home.is_dir())
