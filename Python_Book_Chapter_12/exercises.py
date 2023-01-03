3d0f13a9287a7eab470223b34bc4e0ba0b38aff7
magdazalewska <m.jarzebska.zalewska@gmail.com>
2023-01-03 20:23:13 +0100

    Added Ch12 exercise file

---
+++ Python_Book_Chapter_12/exercises.py
@@ -0,0 +1,14 @@
+import pathlib
+
+path = pathlib.Path(r'C:\Users\mjarzebska004\Downloads\all-coding-books.txt)')
+
+home = pathlib.Path.home()
+print(home)
+print(type(home))
+
+currend_wd = pathlib.Path.cwd()
+print(currend_wd)
+print(type(currend_wd))
+
+
+
