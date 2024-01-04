1f2feb742d71615f86d53916272f77c55c57b65f
Magda Zalewska <m.jarzebska.zalewska@gmail.com>
2024-01-04 01:27:05 +0100

    Added methods to the App class

--- Python_Book_Chapter_18/challenge_18.10.py
+++ Python_Book_Chapter_18/challenge_18.10.py
@@ -1,10 +1,12 @@
 import tkinter as tk
+from random import choice
+from tkinter.filedialog import asksaveasfilename
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title = "Make your own poem!"
+        self.title("Make your own poem!")
         self.rowconfigure([0, 1, 2, 3], minsize=400)
 
         # Frames
@@ -58,34 +60,82 @@ class App(tk.Tk):
         self.lbl_result = tk.Label(master=self.frm_result, text="Your poem: ", pady=10, padx=10)
         self.lbl_result.pack(pady=10)
         # Save as button
-        self.btn_save = tk.Button(master=self.frm_result, text="Save to file")
+        self.btn_save = tk.Button(master=self.frm_result, text="Save to file", command=self.export_poem)
         self.btn_save.pack(side=tk.BOTTOM)
 
-    def validate_entries(self, required_elem_num):
-        """Checks if the number of entries in the entry field is correct. Returns True if it is and False if it isn't"""
+    def _validate_num_entries(self, required_elem_num):
+        """Checks if the number of entries in the entry field is correct. Returns True if it is and False otherwise"""
+        entry_fields = {self.ent_nouns: 3, self.ent_verbs: 3, self.ent_adj: 3, self.ent_adv: 1, self.ent_prep: 3}
+        return all(
+            [False if len(field.get().split(",")) < min_elem else True for field, min_elem in entry_fields.items()])
 
-        # for field, num_elem in entry_fields:
-        #     if len(field.get()) < num_elem:
-        #         field.delete(0, tk.END)
-        #         field.insert(f"The minimum required number of elements in this field is {num_elem}")
+    def _show_warning(self):
+        """Shows a warning if the number of elements in any of the entry fields is below the minimum required number or
+        the elements are not unique"""
+        self.lbl_result["text"] = """
+        Please check your input!
+        I need at least 3 unique nouns, verbs, adjectives, prepositions and one unique adverb to generate a poem for you.
+         """
 
-    def show_warning(self):
-        pass
+    def _check_duplicates(self):
+        """Checks duplicates among the words entered by the user. Returns True if duplicates found and False
+        otherwise"""
+        return all([(False if word_list.count(item) > 1 else True for item in word_list) for word_list in
+                    self._get_word_lists()])
 
-    def check_duplicates(self):
-        """Checks duplicates among the words entered by the user and displays a warning message if any found"""
+    def _get_word_lists(self):
+        verb_list = self.ent_verbs.get().split(",")
+        noun_list = self.ent_nouns.get().split(",")
+        adj_list = self.ent_adj.get().split(",")
+        prep_list = self.ent_prep.get().split(",")
+        adv_list = self.ent_adv.get().split(",")
 
-    def generate_poem(self):
+        return [verb_list, noun_list, adj_list, prep_list, adv_list]
+
+    def _draw(self, elem_list, num_elem):
+        """Draws the required number of unique words"""
+        chosen_elements = [choice(elem_list) for i in range(0, num_elem)]
+        if len(set(chosen_elements)) < 3:
+            self._draw(elem_list, num_elem)
+        else:
+            return chosen_elements
+
+    def _generate_poem(self):
         """Randomly selects at least 3 nouns, 3 verbs, 3 adjectives, 3 prepositions and one adverb from the elements
         provided by the user and generates a poem"""
+        verbs, nouns, adj, prep, adv = self._get_word_lists()
+
+        draw_verbs = self._draw(verbs, 3)
+        draw_nouns = self._draw(nouns, 3)
+        draw_adj = self._draw(adj, 3)
+        draw_prep = self._draw(prep, 3)
+        draw_adv = self._draw(adv, 1)
+
+        vowels = ["a", "e", "i", "o", "u"]
+
+        if adj[0][0] in vowels:
+            self.lbl_result = (f"An {draw_adj[0]} {draw_nouns[0]}\n"
+                               f"An {draw_adj[0]} {draw_nouns[0]} {draw_verbs[0]} {draw_prep[0]} the {draw_adj[1]} {draw_nouns[1]}\n"
+                               f"{draw_adv[0]}, the {draw_nouns[0]} {draw_verbs[1]}"
+                               f"the {draw_nouns[1]} {draw_verbs[2]} {draw_prep[1]} a {draw_adj[2]} {draw_nouns[2]}")
+        else:
+            self.lbl_result = (f"A {draw_adj[0]} {draw_nouns[0]}\n"
+                               f"A {draw_adj[0]} {draw_nouns[0]} {draw_verbs[0]} {draw_prep[0]} the {draw_adj[1]} {draw_nouns[1]}\n"
+                               f"{draw_adv[0]}, the {draw_nouns[0]} {draw_verbs[1]}"
+                               f"the {draw_nouns[1]} {draw_verbs[2]} {draw_prep[1]} a {draw_adj[2]} {draw_nouns[2]}")
 
     def export_poem(self):
         """Saves the poem as a text file"""
+        filepath = asksaveasfilename(defaultextension="txt", filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+        if not filepath:
+            return
+        else:
+            with open(filepath, "w") as file:
+                poem = self.lbl_result["text"]
+                file.write(poem)
 
-
-# window = tk.Tk()
-# window.title("Make your own poem!")
-# window.rowconfigure([0, 1, 2, 3], minsize=400)
+    def main_func(self):
+        pass
 
 
 if __name__ == '__main__':
